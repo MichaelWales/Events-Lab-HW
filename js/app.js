1c7f28d14c50879d102e15cb1f70d466e732bc78
Michael Wales <michaelwales@gmail.com>
2020-04-17 20:58:30 +0100

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const newDoggoForm = document.querySelector('#new-doggo-form');
+  newDoggoForm.addEventListener('submit', handleNewDoggoFormSubmit);
+
+})
+
+const handleNewDoggoFormSubmit = function (event) {
+  event.preventDefault();
+  // console.log(event);
+  const goodListItem = createGoodListItem(event.target);
+  const goodList = document.querySelector('#goodgood-list');
+  goodList.appendChild(goodListItem);
+
+  event.target.reset();
+}
+
+const createGoodListItem = function (form) {
+  const goodListItem = document.createElement('li');
+  goodListItem.classList.add('goodgood-list-item');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  goodListItem.appendChild(name);
+
+  const type = document.createElement('h3');
+  type.textContent = form.type.value;
+  goodListItem.appendChild(type);
+
+  const species = document.createElement('h3');
+  species.textContent = form.species.value;
+  goodListItem.appendChild(species);
+
+  const status = document.createElement('p');
+  status.textContent = form.status.value;
+  goodListItem.appendChild(status);
+
+  // const goodBoyGirl = document.createElement('p');
+  // goodBoyGirl.textContent = form.goodBoyGirl.value;
+  // goodListItem.appendChild(goodBoyGirl);
+
+  return goodListItem;  
+
+}
